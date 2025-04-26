bc3f48bf0198980634f041574ea18242f6551657
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-04-25 20:48:05 -0500

    proyect init

---
+++ LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/Lab3Gtics20212624Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics_20212624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3Gtics20212624Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3Gtics20212624Application.class, args);
+	}
+
+}
---
+++ LAB3_GTICS_20212624/src/test/java/com/example/lab3_gtics_20212624/Lab3Gtics20212624ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab3_gtics_20212624;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3Gtics20212624ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
