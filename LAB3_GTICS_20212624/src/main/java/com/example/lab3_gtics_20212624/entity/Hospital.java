d13b6878fea74237acc076ac23c3386e478b140d
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-05-12 10:45:31 -0500

    no esta funcionando getter y setter de loombok

--- LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/entity/Hospital.java
+++ LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/entity/Hospital.java
@@ -5,8 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+
 @Table(name = "hospital")
 public class Hospital {
 
@@ -18,4 +17,36 @@ public class Hospital {
     private String nombre;
     private String direccion;
     private String telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
