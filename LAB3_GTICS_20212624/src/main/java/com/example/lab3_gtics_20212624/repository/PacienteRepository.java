a30002e8f2402c3c76f14fa6db50b59b6265395c
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-05-12 12:03:03 -0500

    repository

--- LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/repository/PacienteRepository.java
+++ LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/repository/PacienteRepository.java
@@ -1,14 +1,25 @@
-package com.example.lab3_gtics_20212624.repository;
+package com.example.lab3_gtics_20212624.Repository;
 
 import com.example.lab3_gtics_20212624.entity.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
 
-    List<Paciente> findByHospitalId(Integer hospitalId);
+
+
+
+
+
+
+
+
+    @Query("SELECT p FROM Paciente p WHERE p.doctor.id = :doctorId")
+    List<Paciente> listarProximasCitas(@Param("doctorId") Integer doctorId);
+
 }
