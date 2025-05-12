2db019eb0ac578ab46a3fb456ab8917b94b5f754
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-05-12 11:51:20 -0500

    pregunta 5 terminada

---
+++ LAB3_GTICS_20212624/src/main/java/com/example/lab3_gtics_20212624/PacienteRepository/PacienteRepository.java
@@ -0,0 +1,33 @@
+package com.example.lab3_gtics_20212624.PacienteRepository;
+
+import com.example.lab3_gtics_20212624.entity.Paciente;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+
+
+
+
+
+
+
+
+    @Query("SELECT p FROM Paciente p JOIN FETCH p.hospital JOIN FETCH p.doctor")
+    List<Paciente> obtenerPacientesConHospitalYDoctor();
+
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Paciente p SET p.doctor.id = :destinoId WHERE p.doctor.id = :origenId")
+    void derivarPacientesEntreDoctores(@Param("origenId") Integer origenId,
+                                      @Param("destinoId") Integer destinoId);
+
+}
